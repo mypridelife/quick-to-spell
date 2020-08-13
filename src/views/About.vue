45ebbe857a4ce61fbe87a914e7087078a2a0b395
guoyiheng <mypridelife@163.com>
2020-08-13 15:00:25 +0800

    remove unuse

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,37 +1,28 @@
+<!--
+  功能: About
+  作者: gyh
+  版本: v0.1
+-->
 <template>
-  <div class="about ">
-    <h1 class="u-text">This is an about page</h1>
-    <input placeholder="我是红色的！" />
-    1px
-    <div class="mExample"></div>
-    <div class="mExample2"></div>
+  <div class="app-container">
+    page
   </div>
 </template>
 
-<style scoped lang="scss" type="text/css">
-.u-text {
-  font-size: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
 }
-input::placeholder {
-  color: red;
-  font-size: 1.2em;
-  font-style: italic;
-}
+</script>
 
-.mExample {
-  margin: 10px 0;
-  height: 0.5px;
-  width: 500px;
-  background: #333;
-}
-.mExample2 {
-  margin: 10px 0;
-  height: 0.25px;
-  width: 500px;
-  background: #333;
-}
-</style>
+<style scoped lang="scss" type="text/css"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,28 @@
+<!--
+  功能: Home
+  作者: gyh
+  版本: v0.1
+-->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <div class="m-hello">hello world</div>
-    <hello-world />
-    <div class="element">hello world3</div>
+  <div class="app-container">
+    page
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 export default {
-  name: 'Home',
-  components: { HelloWorld }
+  name: '',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
 }
 </script>
+
+<style scoped lang="scss" type="text/css"></style>
