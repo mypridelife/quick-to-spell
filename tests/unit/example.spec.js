e38fa0aa85d20aa24a3b97f639d8f26fcaa8b99a
guoyiheng <mypridelife@163.com>
2020-08-13 17:01:40 +0800

    fix: unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,7 +3,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'hello world2'
+    const msg = 'hello world'
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
